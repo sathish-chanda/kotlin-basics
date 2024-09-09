8fd6333adc579ec5bd7fd99e9efba4fc2068d80b
sathish-chanda <sathishchandas@gmail.com>
2024-09-09 19:41:02 -0400

    basics, data types.

--- hello.kt
+++ hello.kt
@@ -1,5 +1,10 @@
 fun main() {
     println("Hello, Kotlin!")
+    demoValVar()
+    demoDataTypeDeclaration()
+    demoTypeInference()
+    demoStringTemplate()
+    demoIncrementDecrementOperator()
 }
 
 // fun is the keyword - to declare or define a function in kotlin.
@@ -25,3 +30,55 @@ satish@satish-HP-Pavilion-x360-Convertible:~/Projects/kotlin-basics$
 
  */
 
+//  val vs var
+// val is only assigned once and can't be overwritten
+// var can be overwritten its value.
+
+fun demoValVar() {
+    var a: Int = 10
+    println(a)
+    a = 200
+    println(a)
+    val b: Int = 50
+    // b = 100
+    // hello.kt:37:5: error: 'val' cannot be reassigned.
+    // b = 100
+    // ^
+    println(b)
+}
+
+
+fun demoDataTypeDeclaration() {
+    // val/var variableName: DataType = value
+    val firstName: String = "Satish"
+    println(firstName)
+    val year: Int = 2024
+    println(year)
+    // Data Types: String, Int, Float, Double, Boolean
+}
+
+fun demoTypeInference() {
+    // No need of declaring the data type. The compiler can decide the data type based on the assigned value. This make the code look cleaner.
+    var university = "Concordia"
+    println(university)
+}
+
+fun demoStringTemplate() {
+    // we can display the variables in the string using $variableName in the string template. If you want to evaluate an expression then add ${expression}.
+    val language = "Kotlin"
+    val year = 2024
+    println("I am learning $language in the year $year")
+
+    val num1 = 100
+    val num2 = 200
+    println("Sum of $num1 and $num2 is ${num1 + num2}.")
+}
+
+fun demoIncrementDecrementOperator() {
+    var counter = 47
+    println(counter)
+    counter--
+    println(counter)
+    counter++
+    println(counter)
+}
